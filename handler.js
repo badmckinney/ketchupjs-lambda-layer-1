2c4113856da076791d4e3360023899700aa297a8
Brad McKinney <brad.mckin@gmail.com>
2019-03-12 15:18:45 -1000

    remove boiler plate comments

--- handler.js
+++ handler.js
@@ -66,6 +66,4 @@ module.exports.queue = async (event, context, callback) => {
       "input": event,
     }),
   });
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
