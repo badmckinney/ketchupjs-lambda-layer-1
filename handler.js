038cd5d909e0894f4fd3765a92d737f55d77908a
Tyler <alexithemia@gmail.com>
2019-03-14 11:52:52 -1000

    Use parameterized query for security, gets public flag from database and adds to message

--- handler.js
+++ handler.js
@@ -1,11 +1,10 @@
-'use strict';
-
 module.exports.queue = async (event, context, callback) => {
   const aws = require('aws-sdk');
+  const { Client } = require('pg');
   aws.config.update({
     region: 'us-west-2',
-    accessKey: process.env.ACCESS_KEY,
-    secretKey: process.env.SECRET_KEY
+    accessKey: process.env.AWS_ACCESS_KEY_ID,
+    secretKey: process.env.AWS_SECRET_ACCESS_KEY
   });
 
   const sqs = new aws.SQS();
@@ -13,57 +12,82 @@ module.exports.queue = async (event, context, callback) => {
   const queueURL = process.env.QUEUE_URL;
 
   const body = JSON.parse(event.body);
+
+  if (typeof body.value !== 'number') { }
+
   const key = body.key;
   const metric = body.metric;
   const value = body.value;
-  const user_id = body.user_id;
+  const user_name = body.user_name;
 
-  const params = {
-    MessageBody: 'Success',
-    QueueUrl: queueURL,
-    DelaySeconds: 0,
-    MessageAttributes: {
-      key: {
-        DataType: 'String',
-        StringValue: key
-      },
-      metric: {
-        DataType: 'String',
-        StringValue: metric
-      },
-      value: {
-        DataType: 'Number',
-        StringValue: value
-      },
-      user_id: {
-        DataType: 'Number',
-        StringValue: user_id
+  const client = new Client();
+  await client.connect();
+
+  const result = await client.query({
+    text: "SELECT id, public FROM clients WHERE key = $1",
+    values: [key]
+  });
+  await client.end();
+
+  if (result.rows[0]) {
+    const client_id = result.rows[0].id.toString();
+    const public = result.rows[0].public.toString();
+
+    const params = {
+      MessageBody: 'Success',
+      QueueUrl: queueURL,
+      DelaySeconds: 0,
+      MessageAttributes: {
+        client_id: {
+          DataType: 'Number',
+          StringValue: client_id
+        },
+        metric: {
+          DataType: 'String',
+          StringValue: metric
+        },
+        value: {
+          DataType: 'Number',
+          StringValue: value
+        },
+        user_name: {
+          DataType: 'String',
+          StringValue: user_name
+        },
+        public: {
+          DataType: 'String',
+          StringValue: public
+        }
       }
-    },
-    MessageDeduplicationId: 'ketchupDataDeduplicationId',
-    MessageGroupId: 'ketchupData'
+    };
+
+    sqs.sendMessage(params, function (err, data) {
+      if (err) {
+        console.log('error', err);
+        callback('error', {
+          statusCode: 500,
+          body: JSON.stringify({
+            message: "Error",
+            input: event,
+          })
+        });
+      } else {
+        console.log('success');
+      }
+    });
+
+    callback(null, {
+      "statusCode": 200,
+      "body": JSON.stringify({
+        "message": "Successfully sent"
+      }),
+    });
+  } else {
+    callback(null, {
+      "statusCode": 401,
+      "body": JSON.stringify({
+        "message": "Invalid API Key"
+      }),
+    });
   };
-
-  sqs.sendMessage(params, function (err, data) {
-    if (err) {
-      console.log('error', err);
-      callback('error', {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: "Error",
-          input: event,
-        })
-      });
-    } else {
-      console.log('success');
-    }
-  });
-
-  callback(null, {
-    "statusCode": 200,
-    "body": JSON.stringify({
-      "message": "Successfully sent",
-      "input": event,
-    }),
-  });
 };
